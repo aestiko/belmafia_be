aeebdbc43fbde21965ca454f4ced790a9057cab8
Andrei Klimkou <aaklimkov@gmail.com>
2025-09-04 10:44:41 +0300

    feat: add types to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,51 @@
-'use client';
+'use client'
 
-import { gql } from '@apollo/client';
-import { useQuery } from "@apollo/client/react";
-import client from '../lib/apolloClient';
+import type { OperationVariables } from '@apollo/client'
+import { gql } from '@apollo/client'
+import { useQuery } from '@apollo/client/react'
+import client from '../lib/apolloClient'
 
+interface Tournament {
+	id: string
+	name: string
+}
+
+interface GetTournamentsData {
+	tournaments: Tournament[]
+}
+
+// Запрос
 const GET_TOURNAMENTS = gql`
-  query {
-    tournaments {
-      id
-      name
-    }
-  }
-`;
+	query GetTournaments {
+		tournaments {
+			id
+			name
+		}
+	}
+`
 
 export default function Home() {
-  const { data, loading, error } = useQuery(GET_TOURNAMENTS, { client });
+	// useQuery.Result вместо QueryResult
+	const {
+		data,
+		loading,
+		error,
+	}: useQuery.Result<GetTournamentsData, OperationVariables> =
+		useQuery<GetTournamentsData>(GET_TOURNAMENTS, { client })
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error.message}</p>;
+	if (loading) return <p>Loading...</p>
+	if (error) return <p>Error: {error.message}</p>
 
-  return (
-    <div>
-      <h1>Турниры</h1>
-      <ul>
-        {data.tournaments.map((t: any) => (
-          <li key={t.id}>{t.name}</li>
-        ))}
-      </ul>
-    </div>
-  );
+	return (
+		<div>
+			<h1>Турниры</h1>
+			<ul>
+				{(data?.tournaments ?? []).map(tournament => (
+					<li key={(tournament as Tournament).id}>
+						{(tournament as Tournament).name}
+					</li>
+				))}
+			</ul>
+		</div>
+	)
 }
